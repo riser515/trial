b2791f31ca69a9feb1f465ab1bd808cca375c336
Khushi Makhecha <khushi.makhecha@asambhav.org.in>
2022-02-21 13:05:04 +0530

    Initial commit

---
+++ finalServer/routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+// import async from 'hbs/lib/async';
+const ApiRequestService = require('./../services/api.service');
+// import { ApiRequestService } from './../services/api.service';
+
+
+/* GET home page. */
+router.get('/', (req, res)=>{
+  console.log("route / ",JSON.stringify(req.body));
+  res.send("Works");
+});
+
+router.post('/', async (req, res, next) => {
+  console.log("route / ",JSON.stringify(req.body));
+ 
+  const apiService =  new ApiRequestService();
+  if(req.body && req.body.messages){
+  const response = await apiService.postApi('v1/messages',{ 
+    'D360-API-KEY' : process.env.myKey
+  },{
+    'recipient_type' : 'individual',
+    'to' : '917486835085',
+    'type' : 'text',
+    'text' : {
+      "body": "Hey there!"
+        },
+  });
+
+        // res.render('index', { title: 'Express' });
+
+        console.log('response',response);
+    } else if(req.body && req.body.statuses){
+      console.log('status', req.body.statuses[0].status);
+    }
+        // next();
+
+        // res.sendStatus(200);
+      });
+
+module.exports = router;
