5eb4ec79e186b169c5c13a364b3ef40cb0e8ea18
Khushi Makhecha <khushi.makhecha@asambhav.org.in>
2022-02-22 16:27:41 +0530

    used stringify

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.post('/', async (req, res, next) => {
     'to' : '917486835085',
     'type' : 'text',
     'text' : {
-      "body": req.body.messages
+      "body": JSON.stringify(req.body.messages)
         },
   });
 
