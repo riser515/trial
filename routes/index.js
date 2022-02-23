8e9f7781708ebf7968549dbec7dbfcc0d8a33098
Khushi Makhecha <khushi.makhecha@asambhav.org.in>
2022-02-23 11:52:19 +0530

    added await to executing func

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res, next) => {
   if(req.body && req.body.messages){
     const msg = req.body.messages[0].text.body;
   
-    const reply = executeQueries('pizzabot-a9bd', '1', [msg], 'en');
+    const reply = await executeQueries('pizzabot-a9bd', '1', [msg], 'en');
     console.log("Reply: ", reply);
 
   const response = await apiService.postApi('v1/messages',{ 
